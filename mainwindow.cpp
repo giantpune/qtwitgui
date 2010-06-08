5e9410d3973f7bc3057ad60eddbf7e1d8815603c
giantpune <giantpune@1fd8c137-cbf9-692f-1551-38c800d6e7f4>
2010-06-08 17:33:52 +0000

    * Add GPLv3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,13 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <qfiledialog.h>
+/************************************************************************************
+*
+*   - QtWitGui -				2010 giantpune
+*
+*   the multilingual, multiplatform, multiformat gui for messing with
+*   Wii game images.
+*
+*   This software comes to you with a GPLv3 license.
+*   http://www.gnu.org/licenses/gpl-3.0.html
+*
+*   Basically you are free to modify this code, distribute it, use it in
+*   other projects, or anything along those lines.  Just make sure that any
+*   derivative work gets the same license.  And don't remove this notice from
+*   the derivative work.
+*
+*   And please, don't be a douche.  If you borrow code from here, don't claim
+*   you wrote it.  Share your source code with others.  Even if you are
+*   charging a fee for the binaries, let others read the code as somebody has
+*   done for you.
+*
+*************************************************************************************/
+
 #include <QTreeWidget>
 #include <QMessageBox>
 #include <QFontMetrics>
-#include <stdarg.h>
 #include <QStringList>
 #include <qtextstream.h>
+#include <qfiledialog.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
