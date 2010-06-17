260afa20cce6b4e7fdd1d74fe420a877b42ac47a
giantpune <giantpune@1fd8c137-cbf9-692f-1551-38c800d6e7f4>
2010-06-17 19:14:40 +0000

    * add setting to change path for wit * change settings path to "<home directory>/QtWitGui.ini" * except as argv[ 1 ] a path to open on startup

--- trunk/language/witGuiLang_empty.ts
+++ trunk/language/witGuiLang_empty.ts
@@ -414,5 +414,21 @@ Wait for the current job to finish.</source>
         <source> is a cross-platform GUI for wit.&lt;br&gt;This software comes to you with a GPLv3 license&lt;br&gt;&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Maybe cygwin1.dll is missing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Wit appears to have crashed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Done, but with error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error... Check the log</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
