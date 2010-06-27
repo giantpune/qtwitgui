9760500a3b521a1255758b46347e837c5a6c186b
hosigumayuugi <hosigumayuugi@1fd8c137-cbf9-692f-1551-38c800d6e7f4>
2010-06-27 03:37:08 +0000

    * updated Japanese

--- trunk/language/witGuiLang_ja.ts
+++ trunk/language/witGuiLang_ja.ts
@@ -289,7 +289,7 @@
     </message>
     <message>
         <source>Search Path...</source>
-        <translation>パスを検索...</translation>
+        <translation>検索するパス...</translation>
     </message>
     <message>
         <source>Compose Image</source>
@@ -347,7 +347,7 @@ Wait for the current job to finish.</source>
     </message>
     <message>
         <source>Default Region:</source>
-        <translation>リージョン:</translation>
+        <translation>デフォルトリージョン:</translation>
     </message>
     <message>
         <source>AUTO</source>
@@ -541,19 +541,19 @@ GPLv3ライセンスで配布されています
     </message>
     <message>
         <source>Check this box to apply changed of the ID to the TMD and ticket.</source>
-        <translation type="unfinished"></translation>
+        <translation>TMDとチケットにIDの変更を適用するときはこのボックスにチェックを入れてください。</translation>
     </message>
     <message>
         <source>This box displays the region flag of the game that is loaded in the GUI and also allows it to be changed when the game is saved.  This flag is independent of the &quot;region&quot; byte of the ID.</source>
-        <translation type="unfinished"></translation>
+        <translation>このボックスはゲームのリージョンを表示しゲームが保存されたときに変更出来ます。</translation>
     </message>
     <message>
         <source>Check this box to apply changed of the name and ID to the disc header.</source>
-        <translation type="unfinished"></translation>
+        <translation>ディスクヘッダにゲームの名前とIDの変更を適用するときはこのボックスにチェックを入れてください。</translation>
     </message>
     <message>
         <source>Check this box to apply changed of the name and ID to the partition header.</source>
-        <translation type="unfinished"></translation>
+        <translation>領域ヘッダにゲームの名前とIDの変更を適用するときはこのボックスにチェックを入れてください。</translation>
     </message>
     <message>
         <source>This box contains the name of the game as read from 0x20 in the disc header.  This is the name displayed by most homebrew applications.
@@ -562,7 +562,12 @@ To give a game a new name...
 (1) check the &quot;Update Title&quot; box
 (2) Type a name here ( max 63 characters )
 (3) check the &quot;Disc Hdr&quot; box to apply this name to the disc header and/or the &quot;Partition Hdr&quot; box to apply the name to the data patition header.</source>
-        <translation type="unfinished"></translation>
+        <translation>このボックスはディスクヘッダの0x20を名前として読み込んでいます。  この名前はHomebrewアプリで表示されます。
+
+新しい名前を付けるには
+(1) 「ゲーム名を変更」のボックスにチェックを入れる
+(2) ここに文字を入力する(最大63文字)
+(3) 「ディスクヘッダ」にチェックするとディスクヘッダに、「領域ヘッダ」にチェックすると領域ヘッダに名前の変更を適用します。</translation>
     </message>
     <message>
         <source>This box is used for the game&apos;s ID.  The ID is used several places in a game for different reasons.  
@@ -575,38 +580,49 @@ To edit a game ID...
 (1) check the &quot;Update ID&quot; box
 (2) enter an ID in this box ( 6 characters )
 (3) check any combination of the &quot;Disc Hdr&quot;, &quot;Partition Hdr&quot;, and &quot; Tmd / Ticket&quot; boxes to decide where to apply the new ID in the game.</source>
-        <translation type="unfinished"></translation>
+        <translation>このボックスではゲームIDを編集できます。.  ゲームIDは様々な理由で使われます。
+
+HomebrewプログラムはディスクヘッダのIDをゲームを認識するため使います。  これのおかげでディスクヘッダのIDでWBFS領域に存在できます。  0で始まるIDの場合システムメニューによって自動的に起動されます。
+
+TMDとチケットのIDはNANDで使用されます。  セーブデータに使用されるのでIDを変更するとNANDはデータを保存するのに別の場所を使うようになります。
+
+IDを編集するには
+(1) 「IDを変更」のボックスにチェックを入れる
+(2) ここにIDを入力する(最大6文字)
+(3) 「ディスクヘッダ」「領域ヘッダ」「TMD/チケット」から名前の変更を適用する場所を選択してください。</translation>
     </message>
     <message>
         <source>This is the IOS used while the game is running.  Editing in this box is disabled when the &quot;Default IOS&quot; setting is enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>ゲームが起動するときのIOSです。「デフォルトIOS」設定が有効の時はここを編集しても無効になります。</translation>
     </message>
     <message>
         <source>Check the &quot;Update ID&quot; box to allow editing of the game&apos;s ID.</source>
-        <translation type="unfinished"></translation>
+        <translation>「IDを変更」にチェックを入れると変種出来ます。</translation>
     </message>
     <message>
         <source>Check this box to enable editing of a game&apos;s title.</source>
-        <translation type="unfinished"></translation>
+        <translation>ゲーム名を変更するときはこのボックスにチェックを入れてください。</translation>
     </message>
     <message>
         <source>This field is used to display the layout of a game in a collapsible tree.  Not all of the files shown are real files, but instead are an improvised way to deal with metadata present in games.  Likewise, not all of the folder names are real folders and are just made up to deal with metadata files.  Because of this, other tools may list the same files with different names, different paths, or just not even list the file at all.
 
 This basic nomenclature used was first used in SNEEK&apos;s DI and has been adapted to account for files and data not used by that program.</source>
-        <translation type="unfinished"></translation>
+        <translation>ゲームのレイアウトを表示します。リアル名ファイルとして表示されない場合もあります。他のツールでは違う名前、場所になる可能性があります。
+
+SNEEKのDIでこの名名は使用されます。</translation>
     </message>
     <message>
         <source>Select &quot;Test Mode&quot; to build a command and pretend to execute it.  Nothing will be written to the HDD with this box checked.  Use this if you are unsure of a command.</source>
-        <translation type="unfinished"></translation>
+        <translation>HDDに書き込まずコマンドが正しいかチェックするときには「テストモード」を選択してください。</translation>
     </message>
     <message>
         <source>Check the &quot;Overwrite Existing&quot; box to overwrite any conflicting files
 while saving.</source>
-        <translation type="unfinished"></translation>
+        <translation>重複するファイルを上書きする場合は「存在するものを上書きする」にチェックを入れてください。</translation>
     </message>
     <message>
         <source>Many times video game makers blindly run their tool to pack up all their files into a game without thinking about what all they are doing.  If the game has been developed using a svn, this can lead to hundreds of hidden files getting included in the final game that serve no purpose.  Check the &quot;Ignore svn Files in Filetree&quot; box to omit entries containing &quot;/.svn/&quot; in the layout view of the disc.</source>
-        <translation type="unfinished"></translation>
+        <translation>「/.svn/」を隠す場合には「svnファイルを無視する」にチェックを入れてください。</translation>
     </message>
     <message>
         <source>When this box is checked and a game is saved as FST type, it is saved in the format used by the DI module from SNEEK.  This means...
@@ -614,51 +630,55 @@ while saving.</source>
 (1) only the data partition is extracted
 (2) the subfolder &quot;DATA&quot; is omitted
 (3) the &quot;disc&quot; folder and its contents are not extracted</source>
-        <translation type="unfinished"></translation>
+        <translation>このボックスにチェックが入れられている場合ゲームはFST形式で保存され、この形式はSNEEKのDIモジュールで使用されます。これが意味するのは…
+
+(1) データ領域だけ抽出された
+(2) サブフォルダの「DATA」は省かれた
+(3) 「disc」フォルダとその中身は抽出されていない</translation>
     </message>
     <message>
         <source>Select different ammounts of information outputted in the &quot;Output&quot; tab.</source>
-        <translation type="unfinished"></translation>
+        <translation>「出力」タブで違う情報の出力を選択してください。</translation>
     </message>
     <message>
         <source>Use this option to view the disc and partition layout of the game in the &quot;Output&quot; tab.</source>
-        <translation type="unfinished"></translation>
+        <translation>「出力」タブでゲームのディスクと領域のレイアウトを見るときはこの設定を使ってください。</translation>
     </message>
     <message>
         <source>You can check the &quot;Default IOS&quot; checkbox and select a number here to define one IOS to always use when saving a game.  This is useful if you have only a couple IOSes that have the signing bug.
 
 When this box is checked, the IOS selector in the first tab is disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>「デフォルトIOS」にチェックしてIOSの番号を選択すると最初のタブのIOSの選択は無効になります。</translation>
     </message>
     <message>
         <source>Enter a path in &quot;Search Path&quot; to define the starting location for the file browser.  This is used in such things on &quot;open&quot; and &quot;save as&quot;.</source>
-        <translation type="unfinished"></translation>
+        <translation>「検索するパス」に「開く」「名前を付けて保存」に使用される場所を入力してください。</translation>
     </message>
     <message>
         <source>Use the &quot;Start At&quot; setting to decide which tab should be displayed first next time this program runs.</source>
-        <translation type="unfinished"></translation>
+        <translation>「起動時のタブ」でプログラムが起動したときどのタブから始まるか指定できます。</translation>
     </message>
     <message>
         <source>If the &quot;Default Region&quot; box is checked, you can select a region that will be used every time a game is saved.  This does not affect the ID of the game, but only the region flag with in the ISO.
 
 When this box is checked, the region setting in the first tab is disabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>「デフォルトリージョン」にチェックしてリージョンを選択すると最初のタブのリージョンの選択は無効になります。</translation>
     </message>
     <message>
         <source>You can use the &quot;Partition Select&quot; option to define which partitions are both shown in the disc layout tree and saved.</source>
-        <translation type="unfinished"></translation>
+        <translation>「領域を選択」でどの領域が何をしたか分かります。</translation>
     </message>
     <message>
         <source>The &quot;Wit Path&quot; is the path to the wit executable.</source>
-        <translation type="unfinished"></translation>
+        <translation>「Witのパス」はWitが存在する場所のことです。</translation>
     </message>
     <message>
         <source>What&apos;s This...</source>
-        <translation type="unfinished"></translation>
+        <translation>ヘルプ...</translation>
     </message>
     <message>
         <source>F1</source>
-        <translation type="unfinished"></translation>
+        <translation>F1</translation>
     </message>
 </context>
 </TS>
